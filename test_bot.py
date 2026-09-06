257425f205e0c27e6daa98b98aa7ebbe8f877985
Artem <ivanbelkeev708@gmail.com>
2026-09-06 18:48:55 +0800

    Initial commit: Bitrix24 Assistant Bot with Yandex GPT

--- test_bot.py
+++
@@ -1,6 +0,0 @@
-# test_bot.py
-from telegram_bot.bot import TelegramBot
-from config.settings import settings
-
-bot = TelegramBot(settings.get('telegram', 'token'))
-bot.run()
--- test_db.py
+++
@@ -1,5 +0,0 @@
-from database.db_manager import DatabaseManager
-
-db = DatabaseManager()
-user = db.get_or_create_user("123456", "test_user")
-print(f"Created user: {user}")
